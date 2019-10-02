628f60229f15cffc523e4dc62b25ebffbd39607c
Sailesh Dhakal <mesailesh05@gmail.com>
2019-10-02 22:45:19 +0545

    Update billing_software.sql

--- database/billing_software.sql
+++ database/billing_software.sql
@@ -1,1 +1,263 @@
 
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 02, 2019 at 06:49 PM
+-- Server version: 10.1.25-MariaDB
+-- PHP Version: 7.1.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `billing_software`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `customer_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `customer_name` varchar(200) NOT NULL,
+  `customer_address` varchar(250) NOT NULL,
+  `customer_number` bigint(15) NOT NULL,
+  `customer_pan` bigint(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`customer_id`, `user_id`, `created_date`, `customer_name`, `customer_address`, `customer_number`, `customer_pan`) VALUES
+(6, 1, '2019-10-02 21:50:00', 'John', 'Wasington DC, USA', 9851674265, 0),
+(7, 1, '2019-10-02 21:53:07', 'Sam', 'Germany', 8569745215, 0),
+(8, 1, '2019-10-02 21:53:50', 'Dikpal', 'Kathmandu', 9865415786, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order`
+--
+
+CREATE TABLE `invoice_order` (
+  `order_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `customer_id` int(11) NOT NULL,
+  `order_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `order_total_before_tax` decimal(10,2) NOT NULL,
+  `order_total_tax` decimal(10,2) NOT NULL,
+  `order_tax_per` varchar(250) NOT NULL,
+  `order_total_after_tax` double(10,2) NOT NULL,
+  `order_amount_paid` decimal(10,2) NOT NULL,
+  `order_total_amount_due` decimal(10,2) NOT NULL,
+  `note` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_order`
+--
+
+INSERT INTO `invoice_order` (`order_id`, `user_id`, `customer_id`, `order_date`, `order_total_before_tax`, `order_total_tax`, `order_tax_per`, `order_total_after_tax`, `order_amount_paid`, `order_total_amount_due`, `note`) VALUES
+(12, 1, 6, '2019-10-02 16:43:38', '2005.00', '260.65', '13', 2265.65, '1000.00', '65.65', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_order_item`
+--
+
+CREATE TABLE `invoice_order_item` (
+  `order_item_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `item_code` varchar(250) NOT NULL,
+  `item_name` varchar(250) NOT NULL,
+  `order_item_quantity` decimal(10,2) NOT NULL,
+  `order_item_price` decimal(10,2) NOT NULL,
+  `order_item_final_amount` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_order_item`
+--
+
+INSERT INTO `invoice_order_item` (`order_item_id`, `order_id`, `item_code`, `item_name`, `order_item_quantity`, `order_item_price`, `order_item_final_amount`) VALUES
+(4, 11, '15', 'Apple', '5.00', '50.00', '250.00'),
+(5, 12, '26', 'Kingfisher', '3.00', '125.00', '375.00'),
+(6, 12, '28', 'Tuborg', '2.00', '120.00', '240.00'),
+(7, 12, '29', 'Bira91', '2.00', '220.00', '440.00'),
+(8, 12, '30', 'Heineken', '5.00', '190.00', '950.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_user`
+--
+
+CREATE TABLE `invoice_user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `mobile` bigint(20) NOT NULL,
+  `address` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `invoice_user`
+--
+
+INSERT INTO `invoice_user` (`id`, `email`, `password`, `first_name`, `last_name`, `mobile`, `address`) VALUES
+(1, 'admin@dev.com', '123456789', 'Admin', 'Developer', 9876543210, 'Hello World');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE `items` (
+  `item_number` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `item_name` varchar(200) NOT NULL,
+  `item_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `items`
+--
+
+INSERT INTO `items` (`item_number`, `user_id`, `item_name`, `item_price`) VALUES
+(26, 1, 'Kingfisher', '125.00'),
+(27, 1, 'Carlsberg', '120.00'),
+(28, 1, 'Tuborg', '120.00'),
+(29, 1, 'Bira91', '220.00'),
+(30, 1, 'Heineken', '190.00'),
+(31, 1, 'Jameson', '3059.00'),
+(32, 1, 'Magic Moments', '586.00'),
+(33, 1, 'Romanov', '310.00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `receipt`
+--
+
+CREATE TABLE `receipt` (
+  `receipt_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `Customer_id` int(11) NOT NULL,
+  `invoice_id` int(20) NOT NULL,
+  `created_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `amount_paid` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `receipt`
+--
+
+INSERT INTO `receipt` (`receipt_id`, `user_id`, `Customer_id`, `invoice_id`, `created_date`, `amount_paid`) VALUES
+(1, 1, 6, 12, '2019-10-02 16:44:59', '1200.00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`customer_id`);
+
+--
+-- Indexes for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `invoice_customer_key` (`customer_id`);
+
+--
+-- Indexes for table `invoice_order_item`
+--
+ALTER TABLE `invoice_order_item`
+  ADD PRIMARY KEY (`order_item_id`);
+
+--
+-- Indexes for table `items`
+--
+ALTER TABLE `items`
+  ADD PRIMARY KEY (`item_number`);
+
+--
+-- Indexes for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD PRIMARY KEY (`receipt_id`),
+  ADD KEY `foreign_key` (`invoice_id`),
+  ADD KEY `key` (`Customer_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `customer_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+--
+-- AUTO_INCREMENT for table `invoice_order_item`
+--
+ALTER TABLE `invoice_order_item`
+  MODIFY `order_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `items`
+--
+ALTER TABLE `items`
+  MODIFY `item_number` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+--
+-- AUTO_INCREMENT for table `receipt`
+--
+ALTER TABLE `receipt`
+  MODIFY `receipt_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  ADD CONSTRAINT `invoice_customer_key` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `receipt`
+--
+ALTER TABLE `receipt`
+  ADD CONSTRAINT `foreign_key` FOREIGN KEY (`invoice_id`) REFERENCES `invoice_order` (`order_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `key` FOREIGN KEY (`Customer_id`) REFERENCES `customers` (`customer_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
